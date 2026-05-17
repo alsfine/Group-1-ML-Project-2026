8fb6cff435cbc56aacba0a82f1002763d2768797
Raphael Alsfine <raphaelalsfine@gmail.com>
2026-05-17 14:12:30 +0200

    dataset script

--- datasets.sh
+++ datasets.sh
@@ -6,4 +6,4 @@ mkdir -p ./data/processed
 curl -L -o ./data/oulad.zip\
   https://www.kaggle.com/api/v1/datasets/download/anlgrbz/student-demographics-online-education-dataoulad 
 
-unzip ./data/oulad.zip -d ./data/raw/
+#unzip ./data/oulad.zip -d ./data/raw/
